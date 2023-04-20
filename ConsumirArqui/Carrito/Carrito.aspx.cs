6875befa301749e3879612ae07840ddd59e86727
marco <marco@LAPTOP-ISVFJSD0>
2023-04-20 13:59:07 -0600

    Marco y Vane

---
+++ ConsumirArqui/Carrito/Carrito.aspx.cs
@@ -0,0 +1,24 @@
+﻿using ConsumirArqui.Articulos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumirArqui.Carrito
+{
+    public partial class Carrito : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void grCarrito_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtCantidad.Text = grCarrito.SelectedRow.Cells[6].Text.ToString();
+            txtArticulo .Text = grCarrito.SelectedRow.Cells[3].Text.ToString();
+        }
+    }
+}
